94ef4f2a22483bdd99bf321baf7d22747ff8f4ba
Lampx83 <phamxuanlam@gmail.com>
2024-03-17 20:17:05 +0700

    Android Apps

--- app/health/src/main/java/com/example/health/MainActivity.java
+++ app/health/src/main/java/com/example/health/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.health;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -8,82 +9,94 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tempTextView;
-    private TextView pressureTextView;
-    private TextView humidTextView;
-    private TextView proximityTextView;
-    private TextView lightTextView;
-    private TextView magneticTextView;
-    private TextView stepTextView;
-    private Button resetButton;
-    private SensorEventListener sensorListenner;
     private SensorManager sensorManager;
+    private SensorEventCallback sensorListenner;
     private Sensor tempSensor;
-    private Sensor humidSensor;
+    private TextView tempTextView;
     private Sensor pressureSensor;
+    private TextView pressureTextView;
+    private Sensor humidSensor;
+    private TextView humidTextView;
     private Sensor proximitySensor;
+    private TextView proximityTextView;
     private Sensor lightSensor;
+    private TextView lightTextView;
     private Sensor magneticSensor;
-    private Sensor stepSensor;
+    private TextView magneticTextView;
+    private Sensor gyroscopeSensor;
+    private TextView stepTextView;
+    private Sensor accelerometerSensor;
+
     private boolean isWalking = false;
     private int stepCount = 0;
     private static final float STEP_THRESHOLD = 12.0f;
+
+    private Button resetButton;
+    private ProgressBar progressBar;
+    private static final float TARGET = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tempTextView = findViewById(R.id.tempTextView);
-        pressureTextView = findViewById(R.id.pressureTextView);
-        humidTextView = findViewById(R.id.humidTextView);
-        proximityTextView = findViewById(R.id.proximityTextView);
-        lightTextView = findViewById(R.id.lightTextView);
-        magneticTextView = findViewById(R.id.magneticTextView);
-        stepTextView = findViewById(R.id.stepTextView);
-        resetButton = findViewById(R.id.resetButton);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if (tempSensor != null) {
+            tempTextView = findViewById(R.id.tempTextView);
+            tempTextView.setVisibility(View.VISIBLE);
+        }
 
-        //Ambient Temperature
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null)
-            tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if (pressureSensor != null) {
+            pressureTextView = findViewById(R.id.pressureTextView);
+            pressureTextView.setVisibility(View.VISIBLE);
+        }
 
-        //Relative Humidity
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) != null)
-            humidSensor = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        humidSensor = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        if (humidSensor != null) {
+            humidTextView = findViewById(R.id.humidTextView);
+            humidTextView.setVisibility(View.VISIBLE);
+        }
 
-        //Pressure
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null)
-            pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if (proximitySensor != null) {
+            proximityTextView = findViewById(R.id.proximityTextView);
+            proximityTextView.setVisibility(View.VISIBLE);
+        }
 
-        //Proximity
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null)
-            proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (lightSensor != null) {
+            lightTextView = findViewById(R.id.lightTextView);
+            lightTextView.setVisibility(View.VISIBLE);
+        }
 
-        //Light
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null)
-            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (magneticSensor != null) {
+            magneticTextView = findViewById(R.id.magneticTextView);
+            magneticTextView.setVisibility(View.VISIBLE);
+        }
 
-        //Magnetic Field
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null)
-            magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (gyroscopeSensor != null) {
+            stepTextView = findViewById(R.id.stepTextView);
+            ((ViewGroup)stepTextView.getParent()).setVisibility(View.VISIBLE);
+        }
 
-        //        //Gyroscope
-//        if (sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
-//            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-//        }
-
-        //Accelerometer
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null)
-            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometerSensor != null) {
+            stepTextView = findViewById(R.id.stepTextView);
+            ((ViewGroup)stepTextView.getParent()).setVisibility(View.VISIBLE);
+        }
 
         sensorListenner = new SensorEventCallback() {
             @Override
@@ -91,50 +104,42 @@ public class MainActivity extends AppCompatActivity {
                 if (event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
                     float value = event.values[0];
                     if (value <= 100 && value >= -273)
-                        tempTextView.setText("\uD83C\uDF21 \n\n" + String.format("%.0f", value) + "°C");
-                    else tempTextView.setText("\uD83C\uDF21 \n\n" + "0 °C");
+                        tempTextView.setText("🌡 " + String.format("%.0f", value) + "°C");
                 }
                 if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
                     float value = event.values[0];
-                    pressureTextView.setText("\uD83D\uDD5B \n\n" + String.format("%.0f", value) + " hPA");
+                    pressureTextView.setText("🕛 " + String.format("%.0f", value) + "hPA");
                 }
                 if (event.sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY) {
                     float value = event.values[0];
-                    humidTextView.setText("\uD83D\uDCA7 \n\n" + String.format("%.0f", value) + " %");
+                    humidTextView.setText("💧 " + String.format("%.0f", value) + "%");
                 }
-
                 if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
                     float value = event.values[0];
-                    proximityTextView.setText("\uD83D\uDCCF \n\n" + String.format("%.0f", value) + " ");
+                    proximityTextView.setText("📏 " + String.format("%.0f", value) + "cm");
                 }
-
                 if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
                     float value = event.values[0];
-                    lightTextView.setText("\uD83D\uDCA1 \n\n" + String.format("%.0f", value) + " lux");
+                    lightTextView.setText("💡 " + String.format("%.0f", value) + "lux");
                 }
-
                 if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
                     float[] values = event.values;
                     float azimuth = (float) Math.toDegrees(Math.atan2(values[1], values[0]));
                     if (azimuth < 0) azimuth += 360;
-                    magneticTextView.setText("\uD83E\uDDED \n\n" + String.format("%.0f", azimuth) + "°");
+                    magneticTextView.setText("🧭" + String.format("%.0f", azimuth) + "°");
                 }
-
-                if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-                    float xAxis = event.values[0];
-                    float yAxis = event.values[1];
-                    float zAxis = event.values[2];
-                    stepTextView.setText("X: " + xAxis + " rad/s\nY: " + yAxis + " rad/s\nZ: " + zAxis + "  rad/s");
-                }
-
+//                if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+//                    float xAxis = event.values[0];
+//                    float yAxis = event.values[1];
+//                    float zAxis = event.values[2];
+//                    stepTextView.setText("X:" + String.format("%.1f", xAxis) + "\nY: " + String.format("%.1f", yAxis) + "\nZ: " + String.format("%.1f", zAxis));
+//                }
                 if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
                     float x = event.values[0];
                     float y = event.values[1];
                     float z = event.values[2];
-//                    stepTextView.setText("X: " + xAxis + " m/s²\nY: " + yAxis + " m/s²\nZ: " + zAxis + " m/s²");
-
                     // Tính toán gia tốc dọc
-                    float verticalAcceleration = (float) Math.sqrt(x * x + y * y);
+                    float verticalAcceleration = (float) Math.sqrt(x * x + y * y + z * z);
 
                     // Kiểm tra nếu đang đi bộ và có sự thay đổi trong gia tốc độ dọc
                     if (isWalking && verticalAcceleration < STEP_THRESHOLD) {
@@ -143,23 +148,32 @@ public class MainActivity extends AppCompatActivity {
                         isWalking = true;
                         stepCount++;
                         stepTextView.setText("" + stepCount);
+                        progressBar.setProgress((int) ((100 * stepCount) / TARGET));
                     }
                 }
             }
         };
+        resetButton = findViewById(R.id.resetButton);
+        progressBar = findViewById(R.id.progressBar);
+        resetButton.setOnClickListener(v -> {
+            stepCount = 0;
+            stepTextView.setText("0");
+            progressBar.setProgress(0);
+        });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         sensorManager.registerListener(sensorListenner, tempSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(sensorListenner, humidSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(sensorListenner, pressureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorListenner, humidSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(sensorListenner, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(sensorListenner, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(sensorListenner, magneticSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(sensorListenner, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        resetButton.setOnClickListener(v -> {
-            stepCount=0;
-            stepTextView.setText("0");
-        });
+        sensorManager.registerListener(sensorListenner, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorListenner, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
